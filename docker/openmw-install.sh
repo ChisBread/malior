c568c783d5e830e8cce5e874510f7e9e138f1b4d
ChisBread <chisbread@gmail.com>
2022-12-30 11:38:42 +0000

    malior: support systemd

--- docker/openmw-install.sh
+++ docker/openmw-install.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 source $HOME/.config/malior/envs.sh 2>&1 >/dev/null || true
-MALIOR_EXEC_OPT='--user=root' malior '[ ! -f /etc/apt/sources.list.d/openmw-ubuntu-openmw-jammy.list ] && add-apt-repository ppa:openmw/openmw'
-MALIOR_EXEC_OPT='--user=root' malior 'apt update && apt install -y openmw openmw-launcher'
+MALIOR_EXEC_USER=root malior '[ ! -f /etc/apt/sources.list.d/openmw-ubuntu-openmw-jammy.list ] && add-apt-repository ppa:openmw/openmw'
+MALIOR_EXEC_USER=root malior 'apt update && apt install -y openmw openmw-launcher'
--- docker/steam-install.sh
+++ docker/steam-install.sh
@@ -23,10 +23,10 @@ echo "#!/bin/bash
 export STEAMOS=1
 export STEAM_RUNTIME=1
 export DBUS_FATAL_WARNINGS=0
-\$HOME/steam/bin/steam -noreactlogin steam://open/minigameslist $@" > $HOME/.local/malior/bin/steam
+box86 \$HOME/steam/bin/steam -noreactlogin steam://open/minigameslist $@" > $HOME/.local/malior/bin/steam
 chmod +x $HOME/.local/malior/bin/steam
 # update package lists with the newly added arch
-MALIOR_EXEC_OPT='--user=root' malior 'apt update'
+MALIOR_EXEC_USER=root malior 'apt update'
 # install the libraries that Steam requires
-MALIOR_EXEC_OPT='--user=root' malior 'apt install libc6:armhf libncurses5:armhf libsdl2*:armhf libopenal*:armhf libpng*:armhf libfontconfig*:armhf libXcomposite*:armhf libbz2-dev:armhf libXtst*:armhf'
+MALIOR_EXEC_USER=root malior 'apt install -y libc6:armhf libncurses5:armhf libsdl2*:armhf libopenal*:armhf libpng*:armhf libfontconfig*:armhf libXcomposite*:armhf libbz2-dev:armhf libXtst*:armhf'
 echo "Don't forget to 'malior install Box86N64' !"
