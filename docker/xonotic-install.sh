107432f6917510a3f25e7f80a3e3a5c6ab0e6d20
ChisBread <chisbread@gmail.com>
2022-12-30 05:19:18 +0000

    Update: try to fix the sound

--- docker/xonotic-install.sh
+++ docker/xonotic-install.sh
@@ -2,7 +2,6 @@
 source $HOME/.config/malior/envs.sh 2>&1 >/dev/null || true
 [ -d $XONOTIC_DIR ] || git clone https://gitlab.com/xonotic/xonotic.git $XONOTIC_DIR
 
-MALIOR_EXEC_OPT='--user=root' malior 'apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y autoconf automake build-essential curl git libtool libgmp-dev libjpeg-turbo8-dev libsdl2-dev libxpm-dev xserver-xorg-dev zlib1g-dev unzip zip'
 malior 'source /home/player/.config/malior/envs.sh && cd $XONOTIC_DIR && ./all update -l best && ./all compile -r'
 cat > ~/.local/malior/bin/xonotic <<EOF
 #!/bin/env bash
