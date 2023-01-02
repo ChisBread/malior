51046f660fe2921a318c9e69787e62b1672cb4c0
ChisBread <chisbread@gmail.com>
2023-01-02 09:07:04 +0000

    libmali: drop wayland because of https://forum.radxa.com/t/guide-run-minecraft-at-full-speed-on-rock-5b/11937/14

--- docker/libmali-g610-blob-install.sh
+++ docker/libmali-g610-blob-install.sh
@@ -2,18 +2,12 @@ echo "credits to https://gitlab.com/panfork/dri2to3"
 MALIOR_HOME=${MALIOR_HOME:-$HOME}
 source $MALIOR_HOME/.config/malior/envs.sh 2>&1 >/dev/null || true
 mkdir -p $MALIOR_HOME/.local/malior/libmali/x11
-mkdir -p $MALIOR_HOME/.local/malior/libmali/wayland
 
 echo "fetch libmali"
 cd $MALIOR_HOME/.local/malior/libmali/x11
 [ ! -e libmali-valhall-g610-g6p0-x11-gbm.so ] && wget https://github.com/JeffyCN/rockchip_mirrors/raw/libmali/lib/aarch64-linux-gnu/libmali-valhall-g610-g6p0-x11-gbm.so
 ln -sf libmali-valhall-g610-g6p0-x11-gbm.so libmali.so.1
 for l in libEGL.so libEGL.so.1 libgbm.so.1 libGLESv2.so libGLESv2.so.2 libOpenCL.so.1; do ln -sf libmali.so.1 $l; done
-cd $MALIOR_HOME/.local/malior/libmali/wayland
-[ ! -e libmali-valhall-g610-g6p0-wayland-gbm.so ] && wget https://github.com/JeffyCN/rockchip_mirrors/raw/libmali/lib/aarch64-linux-gnu/libmali-valhall-g610-g6p0-wayland-gbm.so
-ln -sf libmali-valhall-g610-g6p0-wayland-gbm.so libmali.so.1
-for l in libEGL.so libEGL.so.1 libgbm.so.1 libGLESv2.so libGLESv2.so.2 libOpenCL.so.1; do ln -sf libmali.so.1 $l; done
-
 
 echo "build dri2to3"
 malior-sudo 'apt-get update && apt-get install -y build-essential meson cmake libxcb-present-dev libxcb-dri2-*dev libxcb-dri3-dev'
