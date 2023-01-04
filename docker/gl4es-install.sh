e263cac395fd771c810d46399fd88568797f107d
Bread <chisbread@gmail.com>
2023-01-04 13:59:48 +0800

    gl4es: remove toolchian

--- docker/gl4es-install.sh
+++ docker/gl4es-install.sh
@@ -1,8 +1,8 @@
-echo "credits to https://gitlab.com/panfork/dri2to3"
+echo "[gl4es] credits to https://gitlab.com/panfork/dri2to3"
 MALIOR_HOME=${MALIOR_HOME:-$HOME}
 source $MALIOR_HOME/.config/malior/envs.sh 2>&1 >/dev/null || true
 
-echo "build gl4es"
+echo "[gl4es] build gl4es"
 sudo apt install 
 malior-sudo 'apt-get update && apt-get install -y  build-essential meson cmake libxcb-present-dev libxcb-dri2-*dev libxcb-dri3-dev'
 malior-sudo 'apt -o Dpkg::Options::="--force-overwrite" --fix-broken install'
@@ -17,7 +17,7 @@ malior '[ "`grep LIB_GL4ES /home/player/.config/malior/envs.sh`" == "" ] && \
 echo "export LIB_GL4ES=/home/player/.local/malior/gl4es/lib64" >> /home/player/.config/malior/envs.sh'
 
 
-echo "build gl4es armhf"
+echo "[gl4es] build gl4es armhf"
 malior-sudo 'apt-get install -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf libxcb-present-dev:armhf libxcb-dri2-*dev:armhf libxcb-dri3-dev:armhf libx11-dev:armhf'
 malior-sudo 'apt-get -o Dpkg::Options::="--force-overwrite" --fix-broken install'
 malior 'cd /home/player/.local/malior/gl4es ; \
@@ -30,7 +30,7 @@ cp -r /home/player/.local/malior/gl4es/lib /home/player/.local/malior/gl4es/lib3
 malior '[ "`grep LIB_GL4ES_32 /home/player/.config/malior/envs.sh`" == "" ] && \
 echo "export LIB_GL4ES_32=/home/player/.local/malior/gl4es/lib32" >> /home/player/.config/malior/envs.sh'
 
-echo "build remove cross build toolchain"
+echo "[gl4es] remove cross build toolchain"
 malior-sudo 'apt-get remove -y gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf'
 malior-sudo 'apt-get -o Dpkg::Options::="--force-overwrite" install -y cpp:armhf cpp-11:armhf'
 echo "      mali blob test: ' MALI_BLOB=x11 malior glmark2'"
